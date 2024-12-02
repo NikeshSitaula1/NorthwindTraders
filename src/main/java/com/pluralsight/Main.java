69d0a4110935719b3e8e1b2d5466cd08754fdcee
Nikesh Sitaula <sitaulanike@gmail.com>
2024-12-02 13:40:29 -0500

    database and java connection

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,16 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
 
+        if (args.length != 2) {
+            System.out.println(
+                    "Application needs two arguments to run: " +
+                            "java com.pluralsight.NorthwindTraders <username> <password>");
+            System.exit(1);
+        }
+        // get the username and password from the command line args
+        String username = args[0];
+        String password = args[1];
+
         try{
             // load the MySQL Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -13,23 +23,27 @@ public class Main {
             Connection connection;
             connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/northwind",
-                    "root",
-                    "yearup");
-        // create statement
-            // the statement is tied to the open connection
-            Statement statement = connection.createStatement();
-        // define your query
-            String query = "SELECT * FROM products";
+                    username,
+                    password);
+
 
         // 2. Execute your query
-            ResultSet results = statement.executeQuery(query);
+            PreparedStatement pStatement = connection.prepareStatement("SELECT * FROM products;");
+
+            ResultSet results = pStatement.executeQuery();
             // process the results
             while (results.next()) {
-                int productId = results.getInt("ProductID");
-                String productName = results.getString("ProductName");
-                System.out.println(productId + ". " + productName);
+                int col1 = results.getInt("productID");
+                String col2 = results.getString("productName");
+                double col3 = results.getDouble("UnitPrice");
+                int col4 = results.getInt("UnitsInStock");
+
+                System.out.println(col1 + ". " + col2 + " | " + col3 + " | " + col4 );
+
             }
         // 3. Close the connection
+            results.close();
+            pStatement.close();
             connection.close();
 
         }catch (ClassNotFoundException e){
@@ -40,6 +54,5 @@ public class Main {
             System.out.println("There was an SQL issue: ");
             e.printStackTrace();
         }
-
     }
 }
